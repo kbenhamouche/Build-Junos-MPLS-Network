101d821ced22aa0faffce1c7d85c9d5a0bb37ce6
listpub <listpub@gmail.com>
2018-05-08 16:06:20 -0400

    script modified

--- Build-MPLS-OSPF-LDP-RR.sh
+++ Build-MPLS-OSPF-LDP-RR.sh
@@ -2,14 +2,14 @@
 # This is an example of bash script to execute all Steps in one script.
 # MPLS deployment for OSPF as IGP, LDP as label protocol, and using Route Reflectors
 # Command example ./Build-MPLS-OSPF-LDP-RR.sh
-printf "Step 1 - IP Addressing in progress.\r"
+printf "Step 1 - IP Addressing in progress.\n"
+sleep 3
 ansible-playbook Step1-GenIPs-pb.yml
+printf "Step 2 - Variables in progress.\n"
 sleep 3
-printf "Step 2 - Variables in progress.\r"
 ansible-playbook Step2-GenVARs-pb.yml
+printf "Step 3 - Configuration in progress.\n"
 sleep 3
-printf "Step 3 - Configuration in progress.\r"
 ansible-playbook Step3-Push-Config-pb.yml
-sleep 3
-printf "The MPLS network is ready.\r"
+printf "The MPLS network is ready.\n"
 sleep 3
