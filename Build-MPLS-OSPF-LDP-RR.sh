461e43729695006d7400cf45c0491d66f1e9f42f
listpub <listpub@gmail.com>
2018-05-08 16:02:27 -0400

    script modified

--- Build-MPLS-OSPF-LDP-RR.sh
+++ Build-MPLS-OSPF-LDP-RR.sh
@@ -4,11 +4,12 @@
 # Command example ./Build-MPLS-OSPF-LDP-RR.sh
 printf "Step 1 - IP Addressing in progress.\r"
 ansible-playbook Step1-GenIPs-pb.yml
-sleep 2
+sleep 3
 printf "Step 2 - Variables in progress.\r"
 ansible-playbook Step2-GenVARs-pb.yml
-sleep 2
+sleep 3
 printf "Step 3 - Configuration in progress.\r"
 ansible-playbook Step3-Push-Config-pb.yml
 sleep 3
-printf "The MPLS network is ready\r"
+printf "The MPLS network is ready.\r"
+sleep 3
